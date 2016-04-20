738b4e6ddb4132614bb74cc0f026cc79480273a8
kimhyunji1 <hjdu21@naver.com>
2016-04-20 15:14:44 +0900

    ..

--- test01/src/Hello2.java
+++ test01/src/Hello2.java
@@ -4,6 +4,7 @@ public class Hello2 {
 	public static void main(String[] args) {
 		System.out.println("test~~1223");
 		System.out.println("okok testest");
+		System.out.println("nono test");
 	}
 
 }
